fbcb614c6a0f2be31663d19a98989e0aa604b0cf
Markel Mencia <markel.mnc@gmail.com>
2025-03-22 16:58:21 +0100

    Add new API calls

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,40 @@
 from fastapi import FastAPI
+from database import *
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
-   return {"message": "Hello World"}
+   return {"message": "askthem"}
+
+
+@app.get("/users/{name}")
+async def query_get_user(name: str):
+   db = sqlite3.connect("db/database.db")
+   user = get_user(db, name)
+   db.close()
+   return {"user": user}
+
+
+@app.get("/users/")
+async def query_get_all_users():
+   db = sqlite3.connect("db/database.db")
+   users = get_all_users(db)
+   db.close()
+   return {"users": users}
+
+
+@app.get("/questions/{id}")
+async def query_get_question(id: int):
+   db = sqlite3.connect("db/database.db")
+   question = get_question_by_id(db, id)
+   db.close()
+   return {"question": question}
+
+
+@app.get("/groups/{name}")
+async def query_get_question(name: str):
+   db = sqlite3.connect("db/database.db")
+   usergroup = get_usergroup(db, name)
+   db.close()
+   return {"group": usergroup}
