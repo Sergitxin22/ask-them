2b52164516d804efce69565f54be511d188f112a
WhiteCatMaster <whitecatsmaster@gmail.com>
2025-07-14 13:18:44 +0200

    objets.py updated

--- backend/objects.py
+++ backend/objects.py
@@ -1,7 +1,9 @@
 class User:
-    def __init__(self, id, name):
+    def __init__(self, id, name, passwd, coins):
         self.id = id
         self.name = name
+        self.passwd = passwd
+        self.coins = coins
 
 class UserGroup:
     def __init__(self, id, name, users):
@@ -9,22 +11,25 @@ class UserGroup:
         self.name = name
         self.users = users
 
-class Question:
-    def __init__(self, id, name, type, options):
+class Using_Question:
+    def __init__(self, id, groupId, questionId, date):
         self.id = id
-        self.name = name
-        self.type = type
-        self.options = options
+        self.groupId = groupId
+        self.questionId = questionId 
+        self.date = date
 
-class Option:
-    def __init__(self, id, name, question_id):
+class Question:
+    def __init__(self, id, question, type):
         self.id = id
-        self.name = name
-        self.question_id = question_id
+        self.question = question
+        self.type = type
 
 class Response:
-    def __init__(self, user_id, question_id, option_id):
+    def __init__(self, user_id, question_id, responseid, usersSelected, date):
         self.id = id
         self.user_id = user_id
         self.question_id = question_id
-        self.option_id = option_id
+        self.responseid = responseid
+        self.usersSelected = usersSelected
+        self.date = date
+
